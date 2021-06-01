1e13555c505ec6092795c7f4f22c263f140ad4ae
Le_Thom <57594269+Le-Thom@users.noreply.github.com>
2021-06-01 08:18:58 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const DiscordRPC = require('discord-rpc'); 
+const client = new DiscordRPC.Client({ transport: 'ipc' }); 
+const config = require('./config.json'); 
+
+if(!config.ID){
+  console.log('\'ID de l\'application n\'est pas renseigné... Veuillez configurer le status dans le fichier config.json')
+  stop()
+}
+
+client.on('ready', async () => {
+  if(!config.state){
+    config.state = "--"
+  }
+  if(!config.details){
+    config.details = "--"
+  }
+  if(!config.largeImageKey){
+    config.largeImageKey = "-"
+  }
+  if(!config.smallImageKey){
+    config.smallImageKey = "-"
+  }
+  if(!config.buttons){
+    await client.setActivity({
+        details: config.details,
+        state: config.state,
+        largeImageKey: config.largeImageKey,
+        smallImageKey: config.smallImageKey
+    }).catch(err => console.log(err));
+  }
+  else{
+    await client.setActivity({ // Setting the Rich Presence Activity based on what is passed in here.
+      buttons: config.buttons,
+      details: config.details,
+      state: config.state,
+      largeImageKey: config.largeImageKey,
+      smallImageKey: config.smallImageKey
+  }).catch(err => console.log(err));
+  }
+    console.log("Votre status à été mis à jour avec succès ! Laissez cette fenêtre ouverte tant que vous voulez afficher votre status\n ATTENTION : Votre activité de jeu doit être activée pour afficher votre status personalisé");
+  });
+
+client.login({ clientId: config.ID }).catch(console.error);
+
+
